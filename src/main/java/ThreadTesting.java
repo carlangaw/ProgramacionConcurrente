c9f60d308be56b586fd760ad94efbaa432eaca74
Carla Nuñez <Carla Nuez@192.168.0.16>
2020-09-02 11:28:42 -0300

    Creo la clase ThreatTesting

---
+++ src/main/java/ThreadTesting.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Carla Nuñez
+ */
+class ThreadTesting {
+
+    public static void main(String[] args) {
+        Thread miHilo = new MiEjecucion();
+        miHilo.start();
+        System.out.println("En el main");
+    }
+}
