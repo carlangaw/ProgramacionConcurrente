d72d4b2010fc384f849d795dfe3d3af03be6fda6
Carla Nuñez <Carla Nuez@192.168.0.16>
2020-09-04 10:36:34 -0300

    Ejercicio 1

---
+++ src/main/java/ej1/testeoRecurso.java
@@ -0,0 +1,23 @@
+package ej1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Carla Nuñez
+ */
+public class testeoRecurso {
+
+    public static void main(String[] args) {
+        Cliente juan = new Cliente();
+        juan.setName("Juan Lopez");
+        Cliente ines = new Cliente();
+        ines.setName("Ines Garcia");
+        juan.start();
+        ines.start();
+    }
+}
